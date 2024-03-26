c63dad47da05a1bc9fe15b6218511d334d79f3e0
Surabhi67 <115413880+Surabhi67@users.noreply.github.com>
2024-03-26 16:17:50 +0530

    lab hours

--- webscraper.py
+++ webscraper.py
@@ -2,9 +2,8 @@ import sqlite3
 import requests
 from bs4 import BeautifulSoup
 import os
-conn=sqlite3.connect("product_sample.db")
-c=conn.cursor()
-# c.execute(""" create table product3(website text, url text, title text,price text) """)
+from amazon import amazon_scrap_ac
+from flipkart import flipkart_scrap_ac
 
 
 def execute_python_file(file_path):
@@ -12,48 +11,13 @@ def execute_python_file(file_path):
       os.system(f'python {file_path}')
    except FileNotFoundError:
       print(f"Error: The file '{file_path}' does not exist.")
-with open('validproxy.txt', "r") as f:
-    proxiesSet=f.read().split("\n")
- 
-count=0
-def amazon_scrap_ac(url):
-    global count
-    
-    while len(proxiesSet) > 0:
-       
-        try:
-            r=requests.get(url,proxies={"http":proxiesSet[count],"https":proxiesSet[count]})
-            print(r.status_code)
-            
-        except:
-            continue
-        finally:
-            count=(count+1)%len(proxiesSet)
-        if r.status_code==200:
-            soup=BeautifulSoup(r.text,'lxml')
-            products=soup.find_all('span',class_="a-declarative")
-            for product in products:
-                prod_title=product.find('span',class_="a-size-medium a-color-base a-text-normal")
-                prod_link=product.find('a',class_="a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal")
-                prod_price=product.find('span',class_="a-price-whole")
-               
-                title= prod_title.text if prod_title is not None else "none"
-                link= prod_link.get('href') if prod_link is not None else "none"
-                
-                price=(prod_price.text) if prod_price is not None else "0"
-                
-                if "none" not in title:
-                    print(f"{title} {link} {price}")
-                    c.execute("insert into product3 values(?,?,?,?)", ("Amazon",link,title,price))
-                    
-                    conn.commit()
-            break
+
+
 execute_python_file("fetchProxy.py")
 print("Execution of fetch complete")
 execute_python_file("proxyServer.py")
 print("Execution of ProxyServer complete")
-search=input("Enter the product to search for")
-base_url="https://www.amazon.in"
-amazon_scrap_ac(base_url+"/s?k="+search)
-conn.close()
+search=input("Enter the product to search for  :  ")
+# amazon_scrap_ac("/s?k="+search)
+flipkart_scrap_ac("/search?q="+search)
 print("Completed successfully")
